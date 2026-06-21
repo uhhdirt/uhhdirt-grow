356e1e865d009bb93891ebc5f8e5ec9478908458
uhhdirt <paulwalkercares@gmail.com>
2026-06-21 12:27:18 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react';
 // If deploying to a SUBDOMAIN (grow.uhhdirt.com) leave base as '/'.
 // If deploying to a SUBPATH/project site (username.github.io/REPO/), set base to '/REPO/'.
 export default defineConfig({
-  base: '/',
+  base: '/uhhdirt-grow/',
   plugins: [react()],
 });
